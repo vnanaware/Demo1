eacfd055e1553b676dde9458453dcfd597afa73c
vishal <vishal@syneoteksoft.com>
2017-01-31 19:53:44 +0530

    Initial Commit

---
+++ DemoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoGit
+//
+//  Created by Bhimashankar Vibhute on 31/01/17.
+//  Copyright © 2017 Syneotek Software Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGit
+//
+//  Created by Bhimashankar Vibhute on 31/01/17.
+//  Copyright © 2017 Syneotek Software Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
